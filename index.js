a61ddd3282424ebd86c7eabd3927983f0721a07e
Honeshwar Thakur <st566654@gmail.com>
2023-01-30 22:34:21 +0530

    adding comments to html and change some css and js

--- index.js
+++ index.js
@@ -14,23 +14,21 @@ let minutesCounter = 0;
 let secondsCounter = 0;
 let COUNTER_STATUS = 'STOPPED';
 
-//creating function to start counter
-function startCounter(){
 
-    // adding event listener to start button of stopwatch
+// adding event listener to start button of stopwatch
     start_btn.addEventListener('click',function (){
         
         //checking counter is start or not
         if(COUNTER_STATUS != "STARTED" ){
             COUNTER_STATUS = "STARTED";
         //initializing stop and reset button event listener when someone click on start button (counter is start) 
-            stopCounter();
+            startCounter();
         }
     });
-}
 
-//creating function to stop and reset counter
- function stopCounter(){
+
+//creating function to start counter
+ function startCounter(){
 
     //changing stop and start button background color and text color on starting of counter
     start_btn.style.backgroundColor='green';
@@ -38,9 +36,10 @@ function startCounter(){
     stop_btn.style.backgroundColor='red';
     stop_btn.style.color='white';
 
-    //setup an function that initialize setInterval 
+    //create an function that initialize setInterval  when ever i call it,
+    // mostly call it after clearInterval to re-initialize setInterval
     let interval;
-    function intervals(){
+    function startTimer(){
      interval = setInterval(function(){
         //setup second hand 
         secondsCounter++;
@@ -50,42 +49,46 @@ function startCounter(){
         else if(secondsCounter>9 && secondsCounter<60){
                 seconds.innerText=secondsCounter;
         }//setup minute hand 
-        else if(secondsCounter === 60 && minutesCounter+1<60 ){
+        else if(secondsCounter === 60){
                 seconds.innerText="00";
                 secondsCounter = 0;
                 minutesCounter++;
+
             if(minutesCounter <= 9){
                 minutes.innerText="0" + minutesCounter+ ':';
-            } else{
+            } 
+            else if(minutesCounter > 9 && minutesCounter<60){
     
                 minutes.innerText=minutesCounter + ':';
-            }
-        }//setup hour hand 
-        else if(minutesCounter === 59 && hoursCounter+1<24){
+            }//setup hour hand 
+            else if(minutesCounter === 60){
                 minutes.innerText="00:";
                 seconds.innerText="00";
                 minutesCounter = 0;
                 secondsCounter = 0;
                 hoursCounter++;
-            if(hoursCounter <= 9){
-                hours.innerText="0" + hoursCounter + ':';
-            } else{
-    
-                hours.innerText=hoursCounter + ':';
-            }
-        }//setup stopwatch end time 23:59:59 after 1 second THAN 00:00:00
-        else if(hoursCounter === 23){
-            hours.innerText="00:";
-            minutes.innerText="00:";
-            seconds.innerText="00";
-            hoursCounter = 0;
-            minutesCounter = 0;
-            secondsCounter = 0;
-        }
+
+                if(hoursCounter <= 9){
+                    hours.innerText="0" + hoursCounter + ':';
+                } 
+                else if(hoursCounter > 9 && hoursCounter<24){
         
+                    hours.innerText=hoursCounter + ':';
+                }// setup stopwatch 00:00:00 when 24 hours completed
+                else if(hoursCounter === 24){
+                    hours.innerText="00:";
+                    minutes.innerText="00:";
+                    seconds.innerText="00";
+                    hoursCounter = 0;
+                    minutesCounter = 0;
+                    secondsCounter = 0;
+                }
+            }
+        }
+       
     },1000);
  }
- intervals();
+ startTimer();
     
 // adding event listener to start button of stopwatch
     let isReset = false;
@@ -102,7 +105,7 @@ function startCounter(){
             //setting stop status(whether counter is stop or not) and depend upon it text on stop button
              STOP_COUNTER_STATUS = "STOPPED"
              stop_btn.innerHTML = '<i class="fa fa-stop"></i>  <span> Stop</span>';  
-             intervals();
+             startTimer();
            }
         });
 
@@ -135,5 +138,3 @@ function startCounter(){
 });
 
  }
-
- startCounter();
